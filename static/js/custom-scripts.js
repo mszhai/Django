a8d74b05996c35f2a9fce7a5c2d2da8657ba0521
mszhai <yingwu_mingren@163.com>
2017-06-25 21:52:55 +0800

    model.html

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -12,7 +12,7 @@
             /*MENU 
             ------------------------------------*/
             $('#main-menu').metisMenu();
-			
+
             $(window).bind("load resize", function () {
                 if ($(this).width() < 768) {
                     $('div.sidebar-collapse').addClass('collapse')
@@ -57,14 +57,14 @@
                 xkey: 'y',
                 ykeys: ['a', 'b'],
                 labels: ['Series A', 'Series B'],
-				 barColors: [
-    '#22a7f0','#1abc9c',
-    '#A8E9DC' 
-  ],
+                barColors: [
+                    '#22a7f0', '#1abc9c',
+                    '#A8E9DC'
+                ],
                 hideHover: 'auto',
                 resize: true
             });
-	 
+
 
 
             /* MORRIS DONUT CHART
@@ -81,10 +81,10 @@
                     label: "Mail-Order Sales",
                     value: 20
                 }],
-				   colors: [
-    '#22a7f0','#1abc9c',
-    '#A8E9DC' 
-  ],
+                colors: [
+                    '#22a7f0', '#1abc9c',
+                    '#A8E9DC'
+                ],
                 resize: true
             });
 
@@ -149,9 +149,9 @@
                 labels: ['iPhone', 'iPad', 'iPod Touch'],
                 pointSize: 2,
                 hideHover: 'auto',
-				  pointFillColors:['#ffffff'],
-				  pointStrokeColors: ['black'],
-				  lineColors:['#22a7f0','#1abc9c'],
+                pointFillColors: ['#ffffff'],
+                pointStrokeColors: ['black'],
+                lineColors: ['#22a7f0', '#1abc9c'],
                 resize: true
             });
 
@@ -160,41 +160,41 @@
             Morris.Line({
                 element: 'morris-line-chart',
                 data: [
-					  { y: '2014', a: 50, b: 90},
-					  { y: '2015', a: 165,  b: 185},
-					  { y: '2016', a: 150,  b: 130},
-					  { y: '2017', a: 175,  b: 160},
-					  { y: '2018', a: 80,  b: 65},
-					  { y: '2019', a: 90,  b: 70},
-					  { y: '2020', a: 100, b: 125},
-					  { y: '2021', a: 155, b: 175},
-					  { y: '2022', a: 80, b: 85},
-					  { y: '2023', a: 145, b: 155},
-					  { y: '2024', a: 160, b: 195}
-				],
-            
-				 
-      xkey: 'y',
-      ykeys: ['a', 'b'],
-      labels: ['Total Income', 'Total Outcome'],
-      fillOpacity: 0.6,
-      hideHover: 'auto',
-      behaveLikeLine: true,
-      resize: true,
-      pointFillColors:['#ffffff'],
-      pointStrokeColors: ['black'],
-      lineColors:['gray','#1abc9c']
-	  
-            });
-           
-        
-            $('.bar-chart').cssCharts({type:"bar"});
-            $('.donut-chart').cssCharts({type:"donut"}).trigger('show-donut-chart');
-            $('.line-chart').cssCharts({type:"line"});
+                    { y: '2014', a: 50, b: 90 },
+                    { y: '2015', a: 165, b: 185 },
+                    { y: '2016', a: 150, b: 130 },
+                    { y: '2017', a: 175, b: 160 },
+                    { y: '2018', a: 80, b: 65 },
+                    { y: '2019', a: 90, b: 70 },
+                    { y: '2020', a: 100, b: 125 },
+                    { y: '2021', a: 155, b: 175 },
+                    { y: '2022', a: 80, b: 85 },
+                    { y: '2023', a: 145, b: 155 },
+                    { y: '2024', a: 160, b: 195 }
+                ],
 
-            $('.pie-thychart').cssCharts({type:"pie"});
-       
-	 
+
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Total Income', 'Total Outcome'],
+                fillOpacity: 0.6,
+                hideHover: 'auto',
+                behaveLikeLine: true,
+                resize: true,
+                pointFillColors: ['#ffffff'],
+                pointStrokeColors: ['black'],
+                lineColors: ['gray', '#1abc9c']
+
+            });
+
+
+            $('.bar-chart').cssCharts({ type: "bar" });
+            $('.donut-chart').cssCharts({ type: "donut" }).trigger('show-donut-chart');
+            $('.line-chart').cssCharts({ type: "line" });
+
+            $('.pie-thychart').cssCharts({ type: "pie" });
+
+
         },
 
         initialization: function () {
@@ -206,20 +206,7 @@
     // Initializing ///
 
     $(document).ready(function () {
-        mainApp.initFunction(); 
-		$("#sideNav").click(function(){
-			if($(this).hasClass('closed')){
-				$('.navbar-side').animate({left: '0px'});
-				$(this).removeClass('closed');
-				$('#page-wrapper').animate({'margin-left' : '260px'});
-				
-			}
-			else{
-			    $(this).addClass('closed');
-				$('.navbar-side').animate({left: '-260px'});
-				$('#page-wrapper').animate({'margin-left' : '0px'}); 
-			}
-		});
+        mainApp.initFunction();
     });
 
 }(jQuery));
